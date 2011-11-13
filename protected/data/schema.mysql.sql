536d4adcbae6ee0f3954b98a3a3407ef7ec938d5
root <root@witali-laptop.(none)>
2011-11-13 18:02:13 +0100

    hello

---
+++ protected/data/schema.mysql.sql
@@ -0,0 +1,31 @@
+CREATE TABLE lookup
+(
+	lookup_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	lookup_name VARCHAR(128) NOT NULL,
+	lookup_code INTEGER NOT NULL,
+	lookup_type VARCHAR(128) NOT NULL,
+	lookup_position INTEGER NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE user
+(
+	user_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_name VARCHAR(128) NOT NULL,
+	user_password VARCHAR(128) NOT NULL,
+	user_salt VARCHAR(128) NOT NULL,
+	user_email VARCHAR(128) NOT NULL,
+	user_profile TEXT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE book
+(
+	book_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	book_title VARCHAR(128) NOT NULL,
+	book_tags TEXT,
+	book_status INTEGER NOT NULL,
+	book_create_time INTEGER,
+	book_update_time INTEGER,
+	owner_id INTEGER NOT NULL,
+	CONSTRAINT FK_book_owner FOREIGN KEY (owner_id)
+		REFERENCES user (user_id) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
